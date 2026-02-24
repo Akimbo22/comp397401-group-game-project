5c14cd69e901d721f67e3aeeaaee502bd94eb1b0
NoelReynolds <nreynol7@my.centennialcollege.ca>
2026-02-24 00:39:59 -0500

    Added basic tower and projectile

---
+++ COMP397-GroupProject/Assets/Scripts/Tower.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] Transform projectileSpawnPos;
+    [SerializeField] float range = 10f;
+    [SerializeField] int maxHealth;
+    [SerializeField] int health;
+    [SerializeField] int projectileDamage;
+    [SerializeField] GameObject projectile;
+    [SerializeField] GameObject baseProjectile;
+    [SerializeField] float baseShotDelay = 10;
+    [SerializeField] float shotDelay = 0;
+    LayerMask enemyLayer;
+
+    void Awake()
+    {
+        enemyLayer = LayerMask.GetMask("EnemyLayer");
+    }
+
+    void Start()
+    {
+        if (maxHealth < 0) maxHealth = 1;
+        health = maxHealth;
+    }
+
+    public void setProjectile(GameObject _projecitle) { projectile = _projecitle; }
+    public void resetProjectile() { projectile = baseProjectile; }
+
+    void FixedUpdate()
+    {
+        if (Physics.Linecast(projectileSpawnPos.position,(projectileSpawnPos.forward * range) + new Vector3(projectileSpawnPos.position.x, projectileSpawnPos.position.y, 0),enemyLayer))
+        {
+            if (shotDelay <= 0)
+            {
+                shotDelay = 0;
+                Shoot();
+                shotDelay = baseShotDelay;
+            }
+        }
+        if (shotDelay > 0) shotDelay -= Time.deltaTime;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!enabled || range <= 0) return;
+        //Gizmos.DrawLine(projectileSpawnPos.position, new Vector3(projectileSpawnPos.position.x, projectileSpawnPos.position.y, projectileSpawnPos.position.z + range));
+        Gizmos.DrawLine(projectileSpawnPos.position, (projectileSpawnPos.forward * range) + new Vector3(projectileSpawnPos.position.x,projectileSpawnPos.position.y,0));
+        //Gizmos.DrawWireCube(new Vector3(projectileSpawnPos.position.x, projectileSpawnPos.position.y, projectileSpawnPos.position.z + (range / 2)), new Vector3(0.5f, 0.5f, range));
+        //Gizmos.DrawWireCube((projectileSpawnPos.position + (projectileSpawnPos.forward * range)) / 2, new Vector3(0.5f, 0.5f, range));
+    }
+
+    public void Shoot()
+    {
+        GameObject shot = GameObject.Instantiate(projectile, projectileSpawnPos.transform.position, projectileSpawnPos.rotation);
+        shot.GetComponent<Rigidbody>().AddForce(projectile.transform.forward * shot.GetComponent<Projectile>().speed, ForceMode.Impulse);
+    }
+
+    public void takeDamage(int damage, string type, bool isNonLethal = false)
+    {
+        //
+
+
+        health -= damage;
+        if (health < 0 && isNonLethal) health = 1;
+        else
+        {
+            OnDeath();
+        }
+    }
+
+    public void OnDeath()
+    {
+
+    }
+}
