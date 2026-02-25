b94a9064f40e52f0400aa53e8aa50c7b72757860
Matthew Elliott <matthewe700@gmail.com>
2026-02-25 06:22:50 -0500

    Added Many UI Elements
    Title screen, game win, game over. Almost fully implemented menus with play game, options, quit game. Includes dialogue and easy way to switch between each menu

---
+++ COMP397-GroupProject/Assets/Scripts/MenuController.cs
@@ -0,0 +1,100 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject startGame;
+    public GameObject options;
+    public GameObject quitGame;
+    //public GameObject optionsWindow;
+    public GameObject talkBanker;
+    public GameObject talkBarkeep;
+    public GameObject talkSheriff;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startGame.SetActive(true);
+        options.SetActive(false);
+        quitGame.SetActive(false);
+        //optionsWindow.SetActive(false);
+        talkBanker.SetActive(false);
+        talkBarkeep.SetActive(false);
+        talkSheriff.SetActive(false);
+    }
+
+    // Remove this comment and add level scene when finished
+    // In the future, add level select
+    //public void OnPlayGame()
+    //{
+    //    SceneManager.LoadScene();
+    //}
+
+    // Waiting to see if professor has method of adjusting volume before implementing this
+    //public void OpenOptions()
+    //{
+    //    optionsWindow.SetActive(true);
+    //}
+
+    public void OnQuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+
+
+    public void SwitchToBanker()
+    {
+        startGame.SetActive(false);
+        options.SetActive(true);
+        quitGame.SetActive(false);
+        //optionsWindow.SetActive(false);
+        talkBanker.SetActive(false);
+        talkBarkeep.SetActive(false);
+        talkSheriff.SetActive(false);
+    }
+
+    public void SwitchToSheriff()
+    {
+        startGame.SetActive(true);
+        options.SetActive(false);
+        quitGame.SetActive(false);
+        //optionsWindow.SetActive(false);
+        talkBanker.SetActive(false);
+        talkBarkeep.SetActive(false);
+        talkSheriff.SetActive(false);
+    }
+
+    public void SwitchToBarkeep()
+    {
+        startGame.SetActive(false);
+        options.SetActive(false);
+        quitGame.SetActive(true);
+        //optionsWindow.SetActive(false);
+        talkBanker.SetActive(false);
+        talkBarkeep.SetActive(false);
+        talkSheriff.SetActive(false);
+    }
+
+    public void TalkBanker()
+    {
+        talkBanker.SetActive(true);
+    }
+
+    public void TalkSheriff()
+    {
+        talkSheriff.SetActive(true);
+    }
+    public void TalkBarkeep()
+    {
+        talkBarkeep.SetActive(true);
+    }
+    public void CloseDialogue()
+    {
+        talkBanker.SetActive(false);
+        talkSheriff.SetActive(false);
+        talkBarkeep.SetActive(false);
+    }
+}
