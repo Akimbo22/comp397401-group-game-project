2ed403f3ac1a80b56dd375cbe7d25d157fa793a1
NoelReynolds <nreynol7@my.centennialcollege.ca>
2026-04-25 17:55:30 -0400

    Touch Controls

--- COMP397-GroupProject/Assets/Scripts/TowerPlacer.cs
+++ COMP397-GroupProject/Assets/Scripts/TowerPlacer.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.EnhancedTouch;
+using EnhancedTouch = UnityEngine.InputSystem.EnhancedTouch;
 using UnityEngine.UI;
+using System.Linq;
 
 public class TowerPlacer : MonoBehaviour
 {
@@ -15,6 +16,7 @@ public class TowerPlacer : MonoBehaviour
     //}
     //public Currency towerCurrency;
     public int towerCost;
+    private bool touchState = false;
     [SerializeField] private GameController gameController;
     [SerializeField] private Object towerPrefab;
     [SerializeField] private RectTransform shopPanelOverlay;
@@ -22,19 +24,29 @@ public class TowerPlacer : MonoBehaviour
     void Start()
     {
         toggle = GetComponent<Toggle>();
+        EnhancedTouch.Touch.onFingerDown += FingerDown;
+        EnhancedTouch.Touch.onFingerUp += FingerUp;
     }
 
+    //Touch requirements
     void Awake()
     {
-        EnhancedTouchSupport.Enable();
-        TouchSimulation.Enable();
+        EnhancedTouch.EnhancedTouchSupport.Enable();
+        EnhancedTouch.TouchSimulation.Enable();
+    }
+
+    //Ditto
+    private void OnDisable()
+    {
+        EnhancedTouch.EnhancedTouchSupport.Disable();
+        EnhancedTouch.TouchSimulation.Disable();
     }
 
     void Update()
     {
         #region Web Controls
         //Web controls
-        /*if (toggle.isOn && Mouse.current.rightButton.wasReleasedThisFrame && gameController.GetGold() >= towerCost && !(shopPanelOverlay.rect.Contains(Mouse.current.position.ReadValue())))
+        /*if (toggle.isOn && Mouse.current.rightButton.wasReleasedThisFrame && gameController.GetGold() >= towerCost && shopPanelOverlay.rect.Contains(Mouse.current.position.ReadValue())==false)
         {
             Ray ray = mainCam.ScreenPointToRay(Mouse.current.position.ReadValue());
             if (Physics.Raycast(ray, out RaycastHit hitPos))
@@ -45,21 +57,27 @@ public class TowerPlacer : MonoBehaviour
             }
         }/**/
         #endregion
+    }
 
-        if (Input.touchCount > 0) Debug.Log("Touch Count non-0");
-        if (Touchscreen.current.primaryTouch.IsPressed()) Debug.Log("Touchscreen Press");
-
+    private void FingerDown(EnhancedTouch.Finger finger)
+    {
         #region Mobile Controls
-        if (Touchscreen.current.primaryTouch.IsPressed() && gameController.GetGold() >= towerCost && !(shopPanelOverlay.rect.Contains(Touchscreen.current.position.ReadValue())))
+        if (toggle.isOn && gameController.GetGold() >= towerCost && touchState==false && shopPanelOverlay.rect.Contains(finger.screenPosition)==false)
         {
-            Ray ray = mainCam.ScreenPointToRay(Mouse.current.position.ReadValue());
+            Ray ray = mainCam.ScreenPointToRay(EnhancedTouch.Touch.fingers[0].screenPosition);
             if (Physics.Raycast(ray, out RaycastHit hitPos))
             {
                 GameObject tower = (GameObject)GameObject.Instantiate(towerPrefab);
                 tower.gameObject.transform.position = new Vector3(hitPos.point.x, 17.41147f, hitPos.point.z);
                 gameController.SetGold(gameController.GetGold() - towerCost);
+                touchState = true;
             }
         }
         #endregion
+    }
+
+    private void FingerUp(EnhancedTouch.Finger finger)
+    {
+        touchState = false;
     }
 }
